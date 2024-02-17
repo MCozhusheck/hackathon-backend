9934d60a95dc3e91d8827cc2e29f35cddf2421bc
Maciej Kożuszek <mackozuszek@gmail.com>
2024-02-17 15:05:18 +0100

    Switch from mongo to postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,5 @@
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
-import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PermitsModule } from './permits/permits.module';
@@ -12,12 +11,14 @@ import { UsersModule } from './users/users.module';
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-      type: 'mongodb',
-      url: 'mongodb://localhost:27017/hackathon-backend',
-      entities: [join(__dirname, '**/**.entity{.ts,.js}')],
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'postgres',
+      entities: [],
       synchronize: true,
-      useNewUrlParser: true,
-      logging: true,
     }),
     ConfigModule.forRoot({
       isGlobal: true,
